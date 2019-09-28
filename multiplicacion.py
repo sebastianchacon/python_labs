152330f55662783915d391674752260b5a5be9a1
Alejandro Chacon <alejandro.chacon@globant.com>
2019-09-28 12:15:59 -0300

    programa multiplicacion

---
+++ multiplicacion.py
@@ -0,0 +1,8 @@
+#mi primer programa en python
+# reciben valores ingresados por el usuario
+a = int(input("ingrese el numero a: "))
+b = int(input("ingrese el numero b: "))
+# operar valores
+c = a + b
+#imprimir valor
+print (c)
