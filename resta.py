e0133bbe4d458936db870ed7b18416df12bdac39
sebas <chaconcarrillosebastian@gmail.com>
2019-09-28 12:46:32 -0300

    programa reparado que resta tres numeros jaja

--- resta.py
+++ resta.py
@@ -3,7 +3,7 @@
 print ("programa que resta tres numeros")
 a = int(input("ingrese el numero a: "))
 b = int(input("ingrese el numero b: "))
-c = int(input("ingrese el numero b: "))
+c = int(input("ingrese el numero c: "))
 # operar valores
 d = a - b - c
 #imprimir valor
