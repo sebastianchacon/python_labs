1e5964ffabe37936da77a4158b2d8583ab093e85
sebas <chaconcarrillosebastian@gmail.com>
2019-09-28 12:43:11 -0300

    hice un programa que resta tres numeros

---
+++ resta.py
@@ -0,0 +1,10 @@
+#programa que resta
+# reciben valores ingresados por el usuario
+print ("programa que resta tres numeros")
+a = int(input("ingrese el numero a: "))
+b = int(input("ingrese el numero b: "))
+c = int(input("ingrese el numero b: "))
+# operar valores
+d = a - b - c
+#imprimir valor
+print ("El resultado de la resta es: "+ str(d))
