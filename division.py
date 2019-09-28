a65fd91fc7445786553a4ad8bce67f59de6f731d
sebas <chaconcarrillosebastian@gmail.com>
2019-09-28 13:31:43 -0300

    programa que divide

--- division.py
+++ division.py
@@ -4,6 +4,6 @@ print ("programa que divide dos numeros")
 a = int(input("ingrese el numero a: "))
 b = int(input("ingrese el numero b: "))
 # operar valores
-c = a // b
+c = a / b
 #imprimir valor
-print ("El resultado de la dividion es: "+ str(c))
+print ("El resultado de la division es: "+ str(c))
