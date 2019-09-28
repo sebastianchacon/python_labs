40e4267194b947f32ac2ba75e9f418ed557b8e8e
sebas <chaconcarrillosebastian@gmail.com>
2019-09-28 13:10:47 -0300

    programa que divide dos numeros

---
+++ division.py
@@ -0,0 +1,9 @@
+#programa que multiplica
+# reciben valores ingresados por el usuario
+print ("programa que multiplica dos numeros")
+a = int(input("ingrese el numero a: "))
+b = int(input("ingrese el numero b: "))
+# operar valores
+c = a * b
+#imprimir valor
+print ("El resultado de la multiplicacion es: "+ str(c))
