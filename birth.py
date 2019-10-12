411600138c966fca23ba385577b3446027ea43ce
Alejandro Chacon <alejandro.chacon@globant.com>
2019-10-12 15:46:21 -0300

    comments

--- birth.py
+++ birth.py
@@ -1,17 +1,25 @@
+#importa libreria para trabajar con funciones de tiempo
 import datetime
-
+#inicializa las variables 
 d, m, y = 0, 0, 0
+
+#almacena la fecha del hoy
 td = datetime.date.today()
 person = 'Your'
+
+# en b se almacena la fecha introducida por el usurio
 b = input('Enter your birth date (DD/MM/YYYY): ')
 # let's establish a SoloLearn-friendly format
 
 try:
     d, m, y = b.split('/') #if that fails, we'll execute the 'except' part
+    #extraes las variables sin /
     d = int(d)
     m = int(m)
     y = int(y)
+    # conviertes la fecha introducida en formato daytime
     bd = datetime.date(y, m, d) # birth date in a datetime format
+    #verificas cuando es tu proximo cumple
     nbd = datetime.date(td.year, m, d) # next birthday
     if (nbd - td).days < 0: # if the ate already passed for this year
         nbd = datetime.date(td.year + 1, m, d)
