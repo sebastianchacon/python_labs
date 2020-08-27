fb84f156b01bd941978043f53d30de8bf9d3e746
sebastianchacon <chaconcarrillosebastian@gmail.com>
2020-08-27 12:24:45 -0300

    update

---
+++ math/par.py
@@ -0,0 +1,10 @@
+a=input("ingrese un numero  ")
+if (a!=0):
+    c = int(a) % 2
+    if (c!=0):
+        print("no es par")
+    else:
+        print("es par")
+else:
+   print("ingrese un numero mayor a 0") 
+
