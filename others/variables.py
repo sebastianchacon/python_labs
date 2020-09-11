f7dc7e8960d2da329b9129c9f0bf85886af0a676
sebastianchacon <chaconcarrillosebastian@gmail.com>
2020-09-11 18:04:11 -0300

    little changes

--- others/variables.py
+++ others/variables.py
@@ -3,9 +3,11 @@ objeto_1 = "Play 3"
 objeto_1_color = "Negro"
 estado_objeto = "Usado"
 pais_origen = "EEUU"
+capacidad = "500gb"
 
 print("Hola, " + nombre_persona)
 print("Estoy muy interesado en tu " + objeto_1)
 print("Es de color " + objeto_1_color)
 print("y tambien esta " + estado_objeto)
 print("Su pais de origen es " + pais_origen)
+print("Tiene una capacidad de " + capacidad)
