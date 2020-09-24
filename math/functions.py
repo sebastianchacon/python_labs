30066caff956bc9b97d1d785d440142bd7227a88
sebastianchacon <chaconcarrillosebastian@gmail.com>
2020-09-24 14:58:12 -0300

    functions

---
+++ math/functions.py
@@ -0,0 +1,11 @@
+def person_1(name,food,drink,dessert)
+    
+name = input("what is your name: ")
+food = input ("what do you want to eat: ")
+drink = input ("what do you want to drink: ")
+dessert = input ("what desert do you want")
+
+print("Hello my name is"+name "i want to eat"+food "i want to drink"+drink "for dessert i want"+dessert)
+
+ 
+
