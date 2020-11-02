a187c09dcae115de9afd79ac9a290295d710fdd9
sebastianchacon <chaconcarrillosebastian@gmail.com>
2020-11-02 20:41:07 -0300

    changes

---
+++ course/section 3/declaracionif.py
@@ -0,0 +1,10 @@
+quiero_comer=False
+quiero_beber=True
+
+if quiero_comer:
+    print("vamos al restaurane!")
+elif quiero_beber:
+    print("vamos a beber!")
+else :
+    print("estoy bien")
+            
---
+++ course/section 3/devolucionderesult.py
@@ -0,0 +1,8 @@
+def calculacion(a,b,c):
+    print(a+b+c)
+    print(a*b/c)
+   
+
+
+calculacion(1,2,3)
+
