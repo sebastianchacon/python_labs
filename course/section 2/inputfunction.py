2c8423bd418893b3465646166fd5cdc8fdce9415
sebastianchacon <chaconcarrillosebastian@gmail.com>
2020-11-02 11:30:32 -0300

    new projects

---
+++ course/section 2/inputfunction.py
@@ -0,0 +1,9 @@
+nombre=input("¿cual es tu nombre?")
+edad=input("¿cual es tu edad?")
+dinero=input("¿cuanto dinero tienes?")
+comida=input("¿que quieres para llevar?")
+
+print("hola, mi nombres es "+nombre)
+print("tengo "+edad)
+print("de dinero tengo "+dinero)
+print("quiero para llevar "+comida)
---
+++ course/section 2/numbermanipulation.py
@@ -0,0 +1,5 @@
+
+
+edad= 50+15
+
+print("hola, mi edad es " + str(edad))
