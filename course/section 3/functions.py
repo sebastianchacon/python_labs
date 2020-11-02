c08583d7342926adc1ef96b8234b481d08a8c8f2
sebastianchacon <chaconcarrillosebastian@gmail.com>
2020-11-02 12:06:03 -0300

    functions

---
+++ course/section 3/functions.py
@@ -0,0 +1,18 @@
+def per_1 (name):
+    print(name+ ": hola, como puedo ayudarte?")
+
+
+def person2_1(food,drink,dessert,name):
+    name=input("cual es tu nombre: ")
+    food=input("que quieres para comer: ")
+    drink=input("que quieres para tomar: ")
+    dessert=input("que quieres de postre: ")
+
+    print(name+ "me gustaria comer"+food "quiero tomar"+drink "quiero de postre"+dessert)
+
+
+def per_1_2(name):
+    print(name+ ": thank you")
+
+
+per1_2("cashier")
