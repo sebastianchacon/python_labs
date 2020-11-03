c16adb17b4e56aeb93ae857775d339440bff37ac
sebastianchacon <chaconcarrillosebastian@gmail.com>
2020-11-03 11:20:56 -0300

    new project

--- course/section 3/declaracionif.py
+++ course/section 3/declaracionif.py
@@ -1,10 +1,9 @@
 quiero_comer=False
-quiero_beber=True
+quiero_beber=False
 
 if quiero_comer:
-    print("vamos al restaurane!")
+    print("vamos al restauran!")
 elif quiero_beber:
     print("vamos a beber!")
 else :
     print("estoy bien")
-            
